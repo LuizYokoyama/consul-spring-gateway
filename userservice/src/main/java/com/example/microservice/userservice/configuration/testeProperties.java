e52dfce10ee8c66d633f0cbfaf5a9b4a87aeddfa
luiz <luizyfs@gmail.com>
2023-04-18 16:29:51 -0300

    37º commit

---
+++ userservice/src/main/java/com/example/microservice/userservice/configuration/testeProperties.java
@@ -0,0 +1,11 @@
+package com.example.microservice.userservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("teste")
+public class testeProperties {
+
+    String teste1;
+
+
+}
